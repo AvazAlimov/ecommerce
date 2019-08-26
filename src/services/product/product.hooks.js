c6a17bfcf9c162b66e1bcdf61719bf5a276334d1
AvazAlimov <avazalimov95@mail.ru>
2019-08-26 13:07:50 +0500

    Product models include added

--- src/services/product/product.hooks.js
+++ src/services/product/product.hooks.js
@@ -1,9 +1,44 @@
+function includeModels(context) {
+  // Get the Sequelize instance. In the generated application via:
+  const sequelize = context.app.get('sequelizeClient');
+  const { brand, category } = sequelize.models;
+
+  context.params.sequelize = {
+    include: [{
+      model: brand,
+      as: 'brand',
+      required: false
+    }, {
+      model: category,
+      as: 'category',
+      required: false
+    }],
+    nest: true,
+  };
+
+  return Promise.resolve(context);
+}
+
+function removeNullModels(context) {
+  if (context.result.data instanceof Array) {
+    context.result.data.forEach(element => {
+      const { brand, category } = element;
+      if (!brand.id)  delete element.brand;
+      if (!category.id)  delete element.category;
+    });
+  } else {
+    const { brand, category } = context.result;
+    if (!brand.id) delete context.result.brand;
+    if (!category.id) delete context.result.category;
+  }
+  return context;
+}
 
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
+    find: [includeModels],
+    get: [includeModels],
     create: [],
     update: [],
     patch: [],
@@ -12,8 +47,8 @@ module.exports = {
 
   after: {
     all: [],
-    find: [],
-    get: [],
+    find: [removeNullModels],
+    get: [removeNullModels],
     create: [],
     update: [],
     patch: [],
