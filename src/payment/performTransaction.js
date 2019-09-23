9433f18f42f9c9ffedb5cadc102ea200f2c02772
AvazAlimov <avazalimov95@mail.ru>
2019-09-23 15:30:31 +0500

    PerformTransaction modified

--- src/payment/performTransaction.js
+++ src/payment/performTransaction.js
@@ -1,4 +1,4 @@
-const { error, ERROR_TRANSACTION_NOT_FOUND, ERROR_INVALID_ACCOUNT } = require('./errors');
+const { error, ERROR_TRANSACTION_NOT_FOUND, ERROR_INVALID_ACCOUNT, ERROR_COULD_NOT_PERFORM } = require('./errors');
 
 module.exports = (app, { params }, res) => {
   app
@@ -7,17 +7,35 @@ module.exports = (app, { params }, res) => {
     .then((orders) => {
       const [order] = orders.data;
       if(order) {
-        order.state = 2;
-        order.performTime = new Date();
-        app.service('orders').update(order.id, order).then(() => {
-          res.status(200).json({
-            result: {
-              'perform_time' : order.performTime.getTime(),
-              'transaction' : order.id.toString(),
-              'state' : order.state,
-            }
+        if(order.state === 1) {
+          order.paid = true;
+          order.state = 2;
+          order.performTime = new Date();
+          app.service('orders').update(order.id, order).then(() => {
+            res.status(200).json({
+              result: {
+                'state' : order.state,
+                'perform_time' : order.performTime.getTime(),
+                'transaction' : order.id.toString(),
+              }
+            });
           });
-        });
+        } else {
+          if (order.state === 2) {
+            order.performTime = new Date();
+            app.service('orders').update(order.id, order).then(() => {
+              res.status(200).json({
+                result: {
+                  'state' : order.state,
+                  'perform_time' : order.performTime.getTime(),
+                  'transaction' : order.id.toString(),
+                }
+              });
+            });
+          } else {
+            error(ERROR_COULD_NOT_PERFORM, res);
+          }
+        }
       } else {
         error(ERROR_TRANSACTION_NOT_FOUND, res);
       }
